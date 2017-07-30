a76553274413c4ca69f4505fb6f56f9a4de35a89
peakcary <peakcary@163.com>
2017-07-30 18:42:35 +0800

    init mongo

--- app.js
+++ app.js
@@ -1,67 +1,48 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var Movie = require('./models/movie')
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000
 var app = express()
 
+// var dbUrl = 'mongodb://localhost/imooc'
+var dbUrl = 'mongodb://127.0.0.1:27017/mydb';
+mongoose.connect(dbUrl)
+
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
 app.use(bodyParser())
-app.use(express.static(path.join(__dirname,'bower_components')))
+app.use(express.static(path.join(__dirname, 'bower_components')))
+app.locals.moment = require('moment')
 app.listen(port)
 
-console.log('server running on  port 3000')
+console.log('server running on  port ' + port)
 
 // index page
 app.get('/', function (req, res) {
-  res.render('index', {
-    title: '首页',
-    movies: [
-      {
-        title: '机械战警',
-        _id: 1,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-      }, {
-        title: '机械战警',
-        _id: 2,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-      }, {
-        title: '机械战警',
-        _id: 3,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-      }, {
-        title: '机械战警',
-        _id: 4,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-      }, {
-        title: '机械战警',
-        _id: 5,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-      }, {
-        title: '机械战警',
-        _id: 6,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+  Movie
+    .fetch(function (err, movies) {
+      if (err) {
+        console.log(err)
       }
-    ]
-  })
+      res.render('index', {
+        title: '首页',
+        movies: movies
+      })
+    })
 })
 
 // detail page
 app.get('/movie/:id', function (req, res) {
-  res.render('detail', {
-    title: '详情页',
-    movie: {
-      doctor: "何塞.博迪利亚",
-      country: "美国",
-      title: "机械战警",
-      year: 2014,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-      language: '英语',
-      flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-      summary: '是一部2014年美国科幻动作片，翻拍自1987年的同名电影，为机械战警系列的第四部作品。巴西导演何塞·帕蒂尔哈执导，乔尔·金纳曼、加里·奥德曼、迈克尔·基顿、塞' +
-          '缪尔·杰克逊、艾比·考尼什和杰基·厄尔·哈利主演。美国和英国于2014年2月14日上映[1]，台湾、香港和中国大陆分别在同年1月31日、2月13日和2月28日上' +
-          '映。'
-    }
+  var id = req.params.id;
+
+  Movie.findById(id, function (err, movie) {
+    res.render('detail', {
+      title: '详情页' + movie.title,
+      movie: movie
+    })
   })
 })
 
@@ -82,24 +63,75 @@ app.get('/admin/movie', function (req, res) {
   })
 })
 
+
+// admin update movie 
+app.get('/admin/update/:id',function(req,res){
+  var id = req.params.id
+  if(id){
+    Movie.findById(id,function(err,movie){
+      res.render('admin',{
+        title:"后台更新",
+        movie:movie
+      })
+    })
+  }
+})
+
+// admin post movie
+app.post('/admin/movie/new', function (req, res) {
+  var id = req.body.movie.id
+  var movieObj = req.body.movie
+  var _movie
+
+  if (id !== 'undefined') {
+    Movie
+      .findById(id, function (err, movie) {
+        if (err) {
+          console.log(err)
+        }
+        _movie = _.extend(movie, movieOjb)
+        _movie.save(function (err, movie) {
+          if (err) {
+            console.log(err)
+          }
+
+          res.redirect('/movie/' + movie._id)
+        })
+      })
+  } else {
+    _movie = new Movie({
+      doctor: movieObj.doctor,
+      title: movieObj.title,
+      country: movieObj.country,
+      language: movieObj.language,
+      year: movieObj.year,
+      poster: movieObj.poster,
+      summary: movieObj.summary,
+      flash: movieObj.flash
+    })
+
+    _movie.save(function (err, movie) {
+      if (err) {
+        console.log(err)
+      }
+
+      res.redirect('/movie/' + movie._id)
+    })
+  }
+})
+
 // list page
 app.get('/admin/list', function (req, res) {
-  res.render('list', {
-    title: '列表页',
-    movies: [
-      {
-        _id:1,
-        doctor: "何塞.博迪利亚",
-        country: "美国",
-        title: "机械战警",
-        year: 2014,
-        poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-        language: '英语',
-        flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-        summary: '是一部2014年美国科幻动作片，翻拍自1987年的同名电影，为机械战警系列的第四部作品。巴西导演何塞·帕蒂尔哈执导，乔尔·金纳曼、加里·奥德曼、迈克尔·基顿、塞' +
-          '缪尔·杰克逊、艾比·考尼什和杰基·厄尔·哈利主演。美国和英国于2014年2月14日上映[1]，台湾、香港和中国大陆分别在同年1月31日、2月13日和2月28日上' +
-          '映。'
+
+  Movie
+    .fetch(function (err, movies) {
+      if (err) {
+        console.log(err)
       }
-    ]
-  })
+      res.render('list', {
+        title: '列表页',
+        movies: movies
+      })
+    })
+
 })
